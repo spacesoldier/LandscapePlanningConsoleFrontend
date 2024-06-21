dcbdfdfbd71d5755ab8bd166ced7d2d63e368232
Vyacheslav Soldatov <kibersoldier@gmail.com>
2024-06-22 03:30:34 +0400

    one more fix

--- src/components/constructor/SidebarWithCta.js
+++ src/components/constructor/SidebarWithCta.js
@@ -151,30 +151,6 @@ export function SidebarWithCta() {
                     Log Out
                 </ListItem>
             </List>
-            <Alert open={openAlert} className="mt-auto" onClose={() => setOpenAlert(false)}>
-                <CubeTransparentIcon className="mb-4 h-12 w-12" />
-                <Typography variant="h6" className="mb-1">
-                    Upgrade to PRO
-                </Typography>
-                <Typography variant="small" className="font-normal opacity-80">
-                    Upgrade to Material Tailwind PRO and get even more components, plugins, advanced features
-                    and premium.
-                </Typography>
-                <div className="mt-4 flex gap-3">
-                    <Typography
-                        as="a"
-                        href="#"
-                        variant="small"
-                        className="font-medium opacity-80"
-                        onClick={() => setOpenAlert(false)}
-                    >
-                        Dismiss
-                    </Typography>
-                    <Typography as="a" href="#" variant="small" className="font-medium">
-                        Upgrade Now
-                    </Typography>
-                </div>
-            </Alert>
         </Card>
     );
 }
