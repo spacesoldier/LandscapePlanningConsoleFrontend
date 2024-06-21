e8917dd507e0b13e8df5f69df6b630726fbf7a70
Vyacheslav Soldatov <kibersoldier@gmail.com>
2024-06-21 20:20:11 +0400

    forgot to pass auth provider instance to system navbar

--- src/components/constructor/ConstructorPage.js
+++ src/components/constructor/ConstructorPage.js
@@ -4,14 +4,14 @@ import {FeaturedImageGallery} from "./ImageGallery";
 import {SidebarWithCta} from "./SidebarWithCta";
 import {SystemNavbar} from "./SystemNavbar";
 
-import { useKeycloak } from "@react-keycloak/web";
+// import { useKeycloak } from "@react-keycloak/web";
 
 function ConstructorPage({authProviderInstance, authInitialized}){
 
 
     return (
         <div className="App">
-            <SystemNavbar />
+            <SystemNavbar authInstance={authProviderInstance} authInitialized={authInitialized}/>
 
             <div class="container">
                 <h1 className="bg-slate-500 text-black text-center">Тут будет список проектов пользователя</h1>
